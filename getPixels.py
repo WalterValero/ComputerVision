aabbb827f1056b8a987c1994686477d37ba65fdd
Walter Valero <walter.valero777@gmail.com>
2015-02-09 04:03:12 -0600

    function getPixels create

--- getPixels.py
+++ getPixels.py
@@ -1,4 +1,9 @@
 from numpy import array
 from PIL import Image
 
+#get pixels from image
+
 def getPixels(image):
+    imagePixels = Image.open(image)
+    pixels = array(imagePixels)
+    return pixels
